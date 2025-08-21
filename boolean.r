d06b0c7d27a430fdd6b15a794487918051bddeb3
Ankita Das <adas22156@gmail.com>
2025-08-21 21:27:04 +0530

    hii

---
+++ boolean.r
@@ -0,0 +1,12 @@
+a <- 200
+b <- 33
+
+if (b < a)
+{
+  print ("b is less than a")
+} else {
+  print("b is not less than a")
+}
+
+9 < 90
+7 < 2
--- escape_char.r
+++ escape_char.r
@@ -7,9 +7,33 @@
 # \f	Form Feed
 # \v	Vertical Tab
 
+# <1>
 str <- "We are the so-called \"Vikings\", from the north."
-
 str
 cat(str, "\n")
 cat("------------------------------")
 
+# <2>
+cat("\nThis is a backslash: \\  \n")
+cat("------------------------------")
+# <3>
+cat("\nLine1\nLine2\n")
+cat("------------------------------")
+# <4>Moves the cursor back to the beginning of the line, without going to the next line.
+# 👉 Explanation: "Hello" is printed, then cursor jumps back to line start (\r), then "Hi" overwrites "He".
+cat("\nHello\rHi\n")
+cat("------------------------------")
+# <5>
+cat("\nName\tAge\nAnkita\t22\n")
+cat("------------------------------")
+# <6>Moves cursor one position back. It erases the previous character (if supported by console).
+cat("\nHello\bWorld\n")
+cat("------------------------------")
+# <7>Historically, this advanced a printer to the next page.
+# In modern terminals, it often behaves like a new line or sometimes shows nothing visible.
+cat("\nPage1\fPage2\n")
+cat("------------------------------")
+# <8> Moves the cursor down to the next vertical tab stop (like multiple new lines).
+# Support varies by terminal.
+cat("\nHello\vWorld\n")
+cat("------------------------------")
